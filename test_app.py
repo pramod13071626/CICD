07f9eeb3954805a71322473c567d3f642d05bc18
Pramod Mothabhau Deore <159231022+pramod13071626@users.noreply.github.com>
2025-02-19 15:31:29 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,4 +4,6 @@ from app import app
 def test_home():
     with app.test_client() as client:
         response = client.get('/')
-        assert response.data == b"Hello, world!"
+        assert response.data == b"Hello, this blog is about Building CI/CD Pipelines with Docker: A Hands-On Guide for Containerized Workflows
+
+!"
