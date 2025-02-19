72996a9f62914395877edfa2eb96113a580509d0
pramod13071626 <othersites141@gmail.com>
2025-02-19 14:24:05 +0530

    Add GitHub Actions workflow for CI/CD

---
+++ README.md
@@ -0,0 +1,1 @@
+CI/CD Pipeline for a Python App using Docker and GitHub Actions
---
+++ test_app.py
@@ -0,0 +1,7 @@
+# test_app.py
+from app import app
+
+def test_home():
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.data == b"Hello, world!"
